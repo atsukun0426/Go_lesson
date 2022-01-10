7474414565b8ac3e970e3cafbd2bce008434e250
atsukun0426 <atsuki0426@gmail.com>
2022-01-10 18:22:43 +0900

    文字列型学習

---
+++ stringtype_lesson.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println("Hello " + "World")
+	fmt.Println(string("Hello World"[0]))
+
+	var s string = "Hello World"
+	s = (strings.Replace(s, "H", "X", 1))
+	fmt.Println(s)
+	fmt.Println(strings.Contains(s, "World"))
+
+	fmt.Println(`Test
+	                 Test
+Trest`)
+
+	// 特殊文字の出力
+	fmt.Println("\"")
+	fmt.Println(`"`)
+}
